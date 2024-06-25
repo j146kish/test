c7354358f79a0d32bfebe050bd2ef1841d37bd83
j146kish <ittachi.mi82@gmail.com>
2024-06-25 14:56:07 +0900

    Update 1.c

--- 1.c
+++ 1.c
@@ -7,7 +7,7 @@ int main(void)
   printf("n = ");
   scanf("%d", &n);
 
-  if( (n & 1) == 0 ){
+  if( (n & 1) == 0 ){ // n & 1 に()をつけ、正しい演算になるように修正しました
     printf("%d は偶数です\n", n);
   }else{
     printf("%d は奇数です\n", n);
